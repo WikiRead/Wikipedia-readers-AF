895990c4bc910573a141cd0a8d310c798a0ec95d
Ramakrishna <2017csb1075@iitrpr.ac.in>
2020-10-26 21:14:29 +0530

    modified:   node_app/app.js 	modified:   node_app/package-lock.json 	modified:   node_app/package.json 	modified:   node_app/views/main.ejs

--- node_app/app.js
+++ node_app/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 const mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
@@ -20,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -43,6 +44,18 @@ connection.on('error', (e) => console.log("error"));
 
 
 // error handler
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
