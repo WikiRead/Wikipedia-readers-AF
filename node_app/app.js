524514fca5fee4a660ae29c2d194a4a78f371d26
yashaswi <2017csb1089@iitrpr.ac.in>
2020-11-01 09:42:48 +0530

    proxy

--- node_app/app.js
+++ node_app/app.js
@@ -1,15 +1,31 @@
 var createError = require('http-errors');
 var express = require('express');
+const session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose')
-
+var cors = require('cors');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 require('dotenv').config();
+
+const sessionConfig = {
+  secret: 'MYSECRET',
+  name: 'appName',
+  resave: false,
+  saveUninitialized: false,
+  cookie : {
+    sameSite: 'none', // THIS is the config you are looing for.
+    secure: false
+  }
+};
+
+app.use(session(sessionConfig));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +35,67 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const API_SERVICE_URL = "https://medium.com/"
+const API_SERVICE_URL1 = "https://www.wikipedia.org/"
+
+app.use('/json_placeholder', createProxyMiddleware({
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  pathRewrite: {
+      [`^/json_placeholder`]: '',
+  },
+  onProxyRes: function (proxyRes, req, res) {
+      console.log(proxyRes);
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+      delete proxyRes.headers['x-frame-options'];
+      //proxyRes.headers['Domain'] = 'http://localhost:4000';
+      // delete proxyRes.headers['set-cookie'];
+      // delete proxyRes.rawHeaders['Set-Cookie'];
+      // // proxyRes.headers['set-cookie'] = proxyRes.headers[
+      // //   "set-cookie"
+      // // ][0].replace("Secure; ", "");
+      // delete proxyRes.headers['cookie'];
+      // delete proxyRes.rawHeaders['Cookie'];
+      // delete proxyRes.headers['cookie2'];
+      // delete proxyRes.headers['x-request-start'];
+      // delete proxyRes.headers['x-request-id'];
+      // delete proxyRes.headers['via'];
+      // delete proxyRes.headers['connect-time'];
+      // delete proxyRes.headers['total-route-time'];
+
+    }
+})
+
+);
+
+
+app.use('/json', createProxyMiddleware({
+  target: API_SERVICE_URL1,
+  changeOrigin: true,
+  pathRewrite: {
+      [`^/json`]: '',
+  },
+  onProxyRes: function (proxyRes, req, res) {
+      console.log("recieved");
+      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+      //proxyRes.headers['Domain'] = 'http://localhost:4000';
+      delete proxyRes.headers['set-cookie'];
+      delete proxyRes.headers['Cookie'];
+      delete proxyRes.headers['cookie2'];
+      // delete proxyRes.headers['x-request-start'];
+      // delete proxyRes.headers['x-request-id'];
+      // delete proxyRes.headers['via'];
+      // delete proxyRes.headers['connect-time'];
+      // delete proxyRes.headers['total-route-time'];
+
+    }
+})
+
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -52,5 +128,8 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+
 
 module.exports = app;
