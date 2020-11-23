c4226de5530014cb7aac697901474662be8cc128
Ramakrishna <2017csb1075@iitrpr.ac.in>
2020-11-23 11:19:03 +0530

    modified:   Analysis/data_analysis.py

--- Analysis/data_analysis.py
+++ Analysis/data_analysis.py
@@ -9,7 +9,7 @@ Created on Sun Nov 22 19:37:03 2020
 
 from data_preprocessing import data_preprocessing 
 from collections import defaultdict
-#from heatmap.py import get_image_data
+from heatmap.py import get_image_data
 
 
 data_path = "data/data.csv"
@@ -64,11 +64,23 @@ for index, row in df_sub.iterrows():
 
 print(avg_time)
 
-#freqs = get_image_data(df)
+freqs = get_image_data(df)
 
 times = []
 
-for index, row in df_sub.iterrows():
-    time = len(df_sub.iloc[index]['gazeX'])*avg_time/1000
-
-print(times)
+for index, row in df.iterrows():
+    time = len(df.iloc[index]['gazeX'])*avg_time/1000
+    times.append(time)
+    
+time_with_pictures = 0
+no_data_with_pictures = 0
+time_without_pictures = 0
+no_data_without_pictures = 0
+for freq in freqs:
+    if freq ==0:
+        no_data_without_pictures+=1
+        time_without_pictures+=1
+    else:
+        no_data_with_pictures+=1
+        time_with_pictures+=1
+    
